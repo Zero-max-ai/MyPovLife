b57b05902a02fccaeda84525b868f7d0e952201d
Zero-max-ai <gangi.keshav03@gmail.com>
2024-05-10 18:48:21 +0530

    backend file with basic structure and working

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,50 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+const userRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string
+    }
+}>();
+
+// get all images
+userRouter.get('/all', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    try {
+        const res = await prisma.images.findMany();
+        c.status(200);
+        return c.json({ res });
+    } catch (err) {
+        c.status(500);
+        return c.json({ message: "error while fetching images!" })
+    }
+
+});
+
+userRouter.get('/image/:id', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    const id = c.req.param('id');
+
+    try {
+        const res = await prisma.images.findFirst({
+            where: {
+                id: id
+            }
+        });
+        c.status(200);
+        return c.json({ res });
+    } catch (err) {
+        c.status(500);
+        return c.json({ message: "error while fetching image!" })
+    }
+
+});
+
+export { userRouter }
